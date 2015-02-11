a2f5e046d19843223176bdd836ce35fe9609de68
Simon Bui <sibui@ucsd.edu>
2015-02-11 07:09:00 +0000

    connection to fake database is working

--- routes/class_page1.js
+++ routes/class_page1.js
@@ -1,5 +1,25 @@
 // Get all of our friend data
 
 exports.view = function(req, res){
-	res.render('class_page1');
+	console.log(req.query.currentCourse);
+	var data = require('../data.json');
+	var index = -1;
+	for(var i=0; i< data.courses.length; i++)
+	{
+		if(data.courses[i].courseName == req.query.currentCourse)
+		{
+			var index = i;
+			break;
+		}
+	}
+	console.log(data.courses[index].author);
+	res.render('class_page1', {"singleCourse":[
+		{
+			"courseName": data.courses[index].courseName,
+			"author": data.courses[index].author,
+			"description": data.courses[index].description,
+			"courseImageURL": data.courses[index].courseImageURL,
+			"courseLink": data.courses[index].courseLink
+		}
+	]});
 };
