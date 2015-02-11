14435dab6125ce20a47aded5331b09433be2d0a9
Simon Bui <sibui@ucsd.edu>
2015-02-11 01:46:53 +0000

    added routes to app.js and route files for all new routes

--- app.js
+++ app.js
@@ -9,7 +9,23 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var home = require('./routes/home');
+var add_a_course = require('./routes/add_a_course');
 var class_page1 = require('./routes/class_page1');
+var course_category = require('./routes/course_category');
+var enrolled_classes = require('./routes/enrolled_classes');
+var help = require('./routes/help');
+var log_out = require('./routes/log_out');
+var logged_in = require('./routes/logged_in');
+var profile = require('./routes/profile');
+var settings = require('./routes/settings');
+var sign_in = require('./routes/sign_in');
+var sign_up = require('./routes/sign_up');
+var student_mode = require('./routes/student_mode');
+var taught_courses = require('./routes/taught_courses');
+var teacher_mode = require('./routes/teacher_mode');
+var teaching_courses = require('./routes/teaching_courses');
+var term_of_use = require('./routes/term_of_use');
+
 // Example route
 // var user = require('./routes/user');
 
@@ -37,7 +53,22 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', home.view);
+app.get('/add_a_course', add_a_course.view);
 app.get('/class_page1', class_page1.view);
+app.get('/course_category', course_category.view);
+app.get('/enrolled_classes', enrolled_classes.view);
+app.get('/help', help.view);
+app.get('/log_out', log_out.view);
+app.get('/logged_in', logged_in.view);
+app.get('/profile', profile.view);
+app.get('/settings', settings.view);
+app.get('/sign_in', sign_in.view);
+app.get('/sign_up', sign_up.view);
+app.get('/student_mode', student_mode.view);
+app.get('/taught_courses', taught_courses.view);
+app.get('/teacher_mode', teacher_mode.view);
+app.get('/teaching_courses', teaching_courses.view);
+app.get('term_of_use', term_of_use.view);
 // Example route
 // app.get('/users', user.list);
 
---
+++ routes/term_of_use.js
@@ -0,0 +1,5 @@
+// Get all of our friend data
+
+exports.view = function(req, res){
+	res.render('term_of_use');
+};
