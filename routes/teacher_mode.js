b93c0fbc3bc2d6a58b591bd8590acf52360badca
Simon Bui <sibui@ucsd.edu>
2015-02-25 00:24:30 +0000

    changed linking after you add course

--- routes/teacher_mode.js
+++ routes/teacher_mode.js
@@ -5,6 +5,7 @@ var data1 = require('../data.json');
 exports.view = function(req, res){
 	var x = data1.courses.length;
 	var data = JSON.parse(JSON.stringify(data1));
+
 	for(var i=x-1; i>-1; i--)
 	{
 		console.log(data.courses[0]);
